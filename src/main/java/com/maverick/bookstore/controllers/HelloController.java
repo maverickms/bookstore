3e6b7d6298e4b09e542dccddb2eb2cde626d1e49
Manvendra Sharma <msharma@paypal.com>
2022-10-16 16:31:36 -0700

    Refactored Hello Controller

--- src/main/java/com/maverick/bookstore/controllers/HelloController.java
+++ src/main/java/com/maverick/bookstore/controllers/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/")
     public String helloMessage(Model model) {
-        return "books/hello";
+        return "hello";
     }
 }
